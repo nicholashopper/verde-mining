b48f2585dc29ebc08e182bbf6d877b4e50ad7ca7
Nicholas <nicholas.l.hopper@gmail.com>
2021-08-17 16:55:47 -0500

    Testing Netlify form bug ...

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -4,24 +4,16 @@
       <h1 class="title">
         Contact
       </h1>
-      <form name="contact" method="POST" data-netlify="true">
-        <div class="items-right">
-          <p>
-            <label>Your Name: <input type="text" name="name" /></label>   
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email" /></label>
-          </p>
-          <!-- <p>
-            <label>Your Role: <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select></label>
-          </p>  -->
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-        </div>
+      <form name="contact" method="POST" netlify>
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>   
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
         <p>
           <button type="submit" class="btn block" >Send</button>
         </p>
