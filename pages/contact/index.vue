b61bfa080a23c1e8e5fbbfe7bdb5a52d9ab9dd0b
Nicholas <nicholas.l.hopper@gmail.com>
2021-08-18 15:54:06 -0500

    Add styling to contact form

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,18 +1,18 @@
 <template>
-  <main>
-    <section class="self-center flex flex-col flex-1 items-center">
-      <h1 class="title">
+  <main class="home-grid">
+    <section class="self-center flex-col items-left contact-form">
+      <h2 class="">
         Contact
-      </h1>
+      </h2>
       <form name="contact" method="POST" netlify>
         <p>
-          <label>Your Name: <input type="text" name="name" /></label>   
+          <label class="mt-1 mb-8 text-primary-600 dark:text-primary-400">Your Name: <input type="text" name="name" /></label>   
         </p>
         <p>
-          <label>Your Email: <input type="email" name="email" /></label>
+          <label class="mt-1 mb-8 text-primary-600 dark:text-primary-400">Your Email: <input type="email" name="email" /></label>
         </p>
         <p>
-          <label>Message: <textarea name="message"></textarea></label>
+          <label class="mt-1 mb-8 text-primary-600 dark:text-primary-400">Message: <textarea name="message"></textarea></label>
         </p>
         <p>
           <button type="submit" class="btn block" >Send</button>
@@ -27,3 +27,25 @@ export default {
 
 }
 </script>
+
+<style>
+.home-grid {
+  background: url("assets/home-grid-v2.svg") no-repeat center bottom;
+  width: 100%;
+}
+
+.contact-form {
+  border-color: var(--border-color);
+  border-width: thin;
+  border-radius: 0.25rem;
+  padding-top: 1rem;
+  padding-right: 1rem;
+  padding-bottom: 1rem;
+  padding-left: 1rem;
+}
+
+input, label, textarea {
+    display: block;
+    width: 100%;
+}
+</style>
