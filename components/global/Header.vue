88678f4f68a4febcd4efe661567b78900a85da9d
Nicholas <nicholas.l.hopper@gmail.com>
2021-08-18 13:41:58 -0500

    Clean up Header Contact button

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -8,7 +8,7 @@
         <nuxt-link class="btn block" to="/projects">Projects</nuxt-link>
       </li>
       <li class="flex-1 ml-2">
-        <nuxt-link class="btn block" to="/contact">Contact Us</nuxt-link>
+        <nuxt-link class="btn block" to="/contact">Contact</nuxt-link>
       </li>
     </ul>
   </nav>
