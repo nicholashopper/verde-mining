52a2c1333add8a253ab74339cf60c3a4aece0590
crashtellini <107076865+crashtellini@users.noreply.github.com>
2022-12-31 11:14:17 -0800

    Adding team page and team member info

---
+++ pages/team/_team.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <h1>Meet Our Team</h1>
+      <div v-for="teamMember in teamMembers" :key="teamMember.name" class="team-member">
+        <img :src="teamMember.avatar" alt="Avatar for {{ teamMember.name }}" class="avatar" />
+        <h3>{{ teamMember.name }}</h3>
+        <p>{{ teamMember.title }}</p>
+      </div>
+    </div>
+  </template>
+
+<script>
+export default {
+  async asyncData({ $content, error }) {
+    let teamMembers
+    try {
+      teamMembers = await $content('team').fetch()
+    } catch (e) {
+      error({ message: "Team members not found" })
+    }
+    return { teamMembers }
+  }
+}
+
+
+</script>
+
+<style>
+.team-member {
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+}
+
+.avatar {
+  width: 500px;
+  height: 500px;
+  border-radius: 50%;
+  margin-right: 1rem;
+}
+</style>
