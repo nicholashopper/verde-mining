3b6241b33928414cb90d164ec0539fdf9c4e7fd6
Daniel Fernandez <dirah9895@gmail.com>
2023-01-13 23:03:35 -0600

    Changed the CSS a bit but mostly fixed some mistakes will my first initial commits

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,6 +1,9 @@
 <template>
     <main>
       <section v-if="posts" class="w-full max-w-5xl mx-auto">
+        <!--
+            Certain styles like 'title' and such will be found in the assets/css folder.
+        -->
         <h1 class="title">Events</h1>
         <posts post-type="events" :amount="10" />
       </section>
